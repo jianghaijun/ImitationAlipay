70ddc9947320f761ca1cf0e5f45427170c2f5a9f
JiangHaiJun <aini1320666709@foxmail.com>
2017-02-07 13:45:38 +0800

    测试Android Studio 2.2.2 更新文件

--- app/src/main/java/com/alipay/imitationalipay/LoginActivity.java
+++ app/src/main/java/com/alipay/imitationalipay/LoginActivity.java
@@ -2,6 +2,7 @@ package com.alipay.imitationalipay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class LoginActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		Log.e("Msg", "123456789");
 	}
 }
